fdaa55e796f847e25b0ade4828eb7828308913f2
hui zheng <huizh365@gmail.com>
2025-03-15 14:52:13 +0100

    Added orders service and manage order page

--- ecommerce-shop/src/pages/ManageOrders.tsx
+++ ecommerce-shop/src/pages/ManageOrders.tsx
@@ -1,7 +1,58 @@
+import { useEffect, useState } from "react"
+import { IOrder } from "../types/Order"
+import { deleteOrder, getOrders } from "../services/orderService"
+import { Link } from "react-router"
+
+
 export const ManageOrders = () => {
+    const [orders, setOrders] = useState<IOrder[]>([])
+
+    useEffect (() => {
+        getOrders().then((data) => setOrders(data))
+    },[])
+
+    const handleDelete = async (id:number) => {
+            await deleteOrder(id)
+            const newOrders = orders.filter(o => +o.id !== id)
+            setOrders(newOrders)
+        }
+
+
     return (
         <>
-        ManageOrders
+        <h2>Manage Orders</h2>
+        <table id="order-list">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Customer</th>
+                <th>Email</th>
+                <th>Total Price</th>
+                <th>Date</th>
+                <th>Payment Status</th>
+                <th>Payment ID</th>
+                <th>Order Status</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                orders.map((o) =>(
+                    <tr className="customer-item" key={o.id}>
+                        <td>{o.id}</td>
+                        <td>{o.customer_firstname} {o.customer_lastname}</td>
+                        <td>{o.customer_email}</td>
+                        <td>{o.total_price}</td>
+                        <td>{o.created_at}</td>
+                        <td>{o.payment_status}</td>
+                        <td>{o.payment_id}</td>
+                        <td>{o.order_status}</td>
+                        <td><Link to={`${o.id}`}>UPDATE </Link> <a href="#" className="delete-link" onClick={()=>handleDelete(+o.id)}> DELETE</a></td>
+                    </tr>
+                ))
+            }
+            </tbody>
+        </table>     
         </>
     )
 }
---
+++ ecommerce-shop/src/services/orderService.ts
@@ -0,0 +1,24 @@
+import { IOrder, OrderCreate, OrderUpdate } from "../types/Order";
+import { BASE_URL, handleRequest } from "./baseService";
+import axios from "axios"
+
+export const getOrders = async ():Promise<IOrder[]> => {
+    return await handleRequest<IOrder[]>(axios.get(`${BASE_URL}orders`))
+}
+
+export const getOrderById = async (id:number):Promise<IOrder> => {
+    return await handleRequest<IOrder>(axios.get(`${BASE_URL}orders/${id}`))
+}
+
+
+export const deleteOrder = async (id:number):Promise<void> => {
+    return await handleRequest<void>(axios.delete(`${BASE_URL}orders/${id}`))
+}
+
+export const updateOrder = async (id:number, payload:OrderUpdate):Promise<IOrder> => {
+    return await handleRequest<IOrder>(axios.patch(`${BASE_URL}orders/${id}`, payload))
+}
+
+export const createOrder = async (payload:OrderCreate):Promise<IOrder> => {
+    return await handleRequest<IOrder>(axios.post(`${BASE_URL}orders`, payload))
+}
