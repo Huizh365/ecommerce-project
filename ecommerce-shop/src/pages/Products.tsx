33078edbe411c4310ae1440ef54d697a49512c99
hui zheng <huizh365@gmail.com>
2025-03-17 15:23:38 +0100

    Added product list page (shop)

--- ecommerce-shop/src/pages/Products.tsx
+++ ecommerce-shop/src/pages/Products.tsx
@@ -1,7 +1,38 @@
+import { useEffect } from "react"
+import { useNavigate } from 'react-router'
+import { useProducts } from "../hooks/useProducts"
+import "../styles/shop.css"
+
 export const Products = () => {
+    const {products, fetchProductsHandler, isLoading, error} = useProducts()
+    const navigate = useNavigate()
+
+    useEffect (() => {
+        fetchProductsHandler()
+    },[])
+
+    const handleClick = (id:number) => {
+        navigate(`/products/${id}`)
+    }
+
     return (
         <>
-        products
+        <div className="filter-wrapper">Filters</div>
+        <div className="products">
+        {
+            products.map((p) =>(
+                <div 
+                    className="product-item" 
+                    key={p.id}
+                    onClick={() => handleClick(p.id)}
+                >
+                    <img src={p.image} alt={p.name}></img>
+                    <p>{p.name}</p>
+                    <p>Price: {p.price} kr</p>
+                </div>
+            ))
+        }
+        </div>
         </>
     )
 }
