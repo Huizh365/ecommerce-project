c19d1b0c9b1ace73a0fcdfb2b24688fbc3d9bcb4
hui zheng <huizh365@gmail.com>
2025-03-18 15:08:57 +0100

    added admin navbar

--- ecommerce-shop/src/components/AdminNav.tsx
+++ ecommerce-shop/src/components/AdminNav.tsx
@@ -1,7 +1,26 @@
+import logo from "../assets/logo.png"
+import "../styles/nav.css"
+
 export const AdminNav = () => {
+
+
     return (
-        <>
-        Admin nav
-        </>
+        <nav className="navbar">
+            <div className="nav-left">
+                <img src={logo} alt="snacktpia logo" className="nav-logo" />
+            </div>
+
+            <div className="nav-links">
+                <a href="/admin/orders" className="navbar-link">ORDERS</a>
+                <a href="/admin/customers" className="navbar-link">CUSTOMERS</a>
+                <a href="/admin/products" className="navbar-link">PRODUCTS</a>
+            </div>
+
+            <div className="nav-right">
+                <a id="user-enter" href="/admin" className="navbar-link">Log out</a>
+            </div>
+
+
+        </nav>
     )
 }
