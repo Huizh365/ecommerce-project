0a106bcfc2969f9dd3df54c588f9faaa30b8e33d
hui zheng <huizh365@gmail.com>
2025-03-18 12:24:30 +0100

    added addToCart function on product details page, added hook useCart

--- ecommerce-shop/src/pages/ProductDetails.tsx
+++ ecommerce-shop/src/pages/ProductDetails.tsx
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react"
 import { useProducts } from "../hooks/useProducts"
-import { useNavigate, useParams, Link } from 'react-router'
+import { useParams, Link } from 'react-router'
 import { IProduct } from "../types/Product"
 import "../styles/shop.css"
+import { useCart } from "../hooks/useCart"
+import { ICartActionType } from "../reducers/CartReducer"
+import { CartItem } from "../models/CartItem"
 
 
 export const ProductDetails = () => {
     const {fetchProductByIdHandler, isLoading, error} = useProducts()
-    const navigate = useNavigate()
     const params = useParams()
     const [product, setProduct] = useState<IProduct | null>(null)
+
+    const {cart, dispatch} = useCart()
 
     useEffect(()=>{
         if(!params.id) return
         fetchProductByIdHandler(+params.id).then((data) => setProduct(data))
     },[])
+
+
+    const addToCart = (product: IProduct, quantity: number) => {
+        dispatch({
+            type: ICartActionType.ADD_ITEM,
+            payload: new CartItem(product, quantity)
+        })
+    }
 
     return (
         <>
@@ -26,7 +38,10 @@ export const ProductDetails = () => {
                 <h4>{product?.price} kr</h4>
                 <p>{product?.description}</p>
                 {product?.stock ? <p className="in-stock">In Stock</p> : <p className="out-stock">Out of Stock</p>}
-                <button className="add-btn">Add to Cart</button>
+                <button className="add-btn" 
+                    onClick={() => {product && addToCart(product, 1)}}
+                    disabled={!product || product.stock === 0}
+                >Add to Cart</button>
             </div>
         </div>
         </>
