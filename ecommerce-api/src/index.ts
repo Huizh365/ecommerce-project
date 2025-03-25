7dc40eca8a6c890e9fa57b40c9b520a8dbb52fca
hui zheng <huizh365@gmail.com>
2025-03-25 21:38:06 +0100

    tested stripe and CLI

--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import express from "express";
+import express,  { Request, Response } from "express";
 import {connectDB} from "./config/db";
 import cors from "cors";
 
@@ -20,6 +20,62 @@ app.use('/customers', customerRouter)
 app.use('/orders', orderRouter)
 app.use('/order-items', orderItemRouter)
 
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+app.post('/stripe/create-checkout-session-embedded', async (req: Request, res: Response) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: 'SEK',
+          product_data: {
+            name: 'Rikards Tårta',
+            images: ['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fusercontent.one%2Fwp%2Fwww.foodbylaven.se%2Fwp-content%2Fuploads%2F2020%2F10%2FDBB2DADD-264D-4003-9BD5-245331785548-1300x1300.jpg&f=1&nofb=1&ipt=260d639620b12c5ea5985f74668df5b56cde82438205e5a518014ebf502ea279&ipo=images']
+          },
+          unit_amount: 5 * 100,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    ui_mode: 'embedded',
+    return_url: 'http://localhost:5173/order-confirmation?session_id={CHECKOUT_SESSION_ID}',
+    client_reference_id: '123'
+  });
+
+  res.send({clientSecret: session.client_secret});
+});
+
+
+
+app.post('/stripe/webhook', (req: Request, res: Response) => {
+  const event = req.body;
+
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.completed':
+      const session = event.data.object;
+      console.log(session);
+      
+      // Update order with confirmed payment
+      // -- payment_status = "Paid"
+      // -- payment_id = session.id
+      // -- order_status = "Received"
+
+      // Update product stock
+
+      // Send confirmation email to customer
+
+      // Sen purchase to accounting service
+      console.log(event.type.object);
+   
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a response to acknowledge receipt of the event
+  res.json({received: true});
+});
 
 // Attempt to connect to the database
 connectDB()
--- ecommerce-shop/src/pages/Checkout.tsx
+++ ecommerce-shop/src/pages/Checkout.tsx
@@ -1,8 +1,41 @@
+import * as React from 'react'
+import {loadStripe} from '@stripe/stripe-js'
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout
+} from '@stripe/react-stripe-js'
+
+const stripePromise = loadStripe('pk_test_51R4GtGLXMXfp1X0Luq3XRc5atnzyrApfXMCRVHgvPCDZtZxt13xgOkDTfqhYJKxZwtLMStsHzpttDCQJdkioGqjY00ZdahA5zH')
 
 export const Checkout = () => {
-    return (
-        <>
-        check out
-        </>
-    )
+    const fetchClientSecret = React.useCallback(() => {
+        // Create a Checkout Session
+        return fetch("http://localhost:3000/stripe/create-checkout-session-embedded", {
+          method: "POST",
+        })
+          .then((res) => res.json())
+          .then((data) => data.clientSecret);
+      }, []);
+    
+      const options = {fetchClientSecret};
+    
+      return (
+        <div id="checkout">
+          <h1>Checkout (Embedded)</h1>
+          
+          <h3>Varukorg</h3>
+          <p>...</p>
+    
+          <h3>Kund info (formulär)</h3>
+          <p>...</p>
+    
+          <h3>Betalning</h3>
+          <EmbeddedCheckoutProvider
+            stripe={stripePromise}
+            options={options}
+          >
+            <EmbeddedCheckout />
+          </EmbeddedCheckoutProvider>
+        </div>
+      )
 }
